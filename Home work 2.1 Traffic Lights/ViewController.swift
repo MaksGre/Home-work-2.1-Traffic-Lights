389e594769a0f5caa570f74e11cbff547bfd103f
Maksim Grebenozhko <maks_gr@rambler.ru>
2019-07-23 13:31:28 +0400

    Initial Commit

---
+++ Home work 2.1 Traffic Lights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home work 2.1 Traffic Lights
+//
+//  Created by Maksim Grebenozhko on 23/07/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
